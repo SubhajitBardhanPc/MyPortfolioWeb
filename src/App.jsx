65aa7972b2d20656c3293421c2ed684950c28c17
SubhajitBardhanPc <bardhansubhajit3@gmail.com>
2025-06-22 08:40:48 +0530

    Fresh start: initialized portfolio-app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./index.css";
+import Navbar from "./components/Navbar/Navbar.jsx";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      {/* Add your sections here */}
+    </>
+  );
+}
+
+export default App;
